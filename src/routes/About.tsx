17d4bb7739f54baf34ab995f0644c68b239fc6f3
JorgeMioska <quispechilojorge@gmail.com>
2023-09-14 23:44:41 -0500

    height fixed

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -5,7 +5,7 @@ import { Clients, Team } from "../components";
 export const About = () => {
   return (
     <>
-      <section className="min-h-vh flex gap-10 items-center">
+      <section className="min-h-screen md:min-h-vh flex gap-10 items-center">
         <div className="flex flex-col gap-10">
           <h2 className="text-6xl">About Us</h2>
           <span className="font-light text-lg">
