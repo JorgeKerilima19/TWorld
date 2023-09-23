da31c3e51ebf02877b82863287db44173319e678
JorgeMioska <quispechilojorge@gmail.com>
2023-09-22 22:12:02 -0500

    error page

--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,3 +1,33 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
+import errorImg from "/error.svg";
+
 export const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className="h-screen flex flex-col md:flex-row gap-10 items-center justify-center">
+      <img
+        src={errorImg}
+        alt="errorImg"
+        className="absolute opacity-5 h-4/5 m-auto pointer-events-none"
+      />
+      <span className="text-8xl font-light md:py-5 md:pr-10 md:border-r-4 md:border-r-red-700">
+        404
+      </span>
+      <div className="grid gap-9 place-items-center md:place-items-start">
+        <div className="max-h-56 flex gap-2 items-center justify-center flex-wrap">
+          <img src={logo} alt="logo" className="h-10" />
+          <h2 className="text-xl font-bold">Tworld</h2>
+        </div>
+        <span className="text-3xl font-light text-center">
+          Sorry this page is not available
+        </span>
+        <Link
+          className=" bg-red-700 text-white px-8 py-2 text-lg rounded-md  "
+          to="/"
+        >
+          <button>Go back Home</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
