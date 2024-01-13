f3636bf8711bf6735ddb588ced9ecbd06a4f6e5a
JorgeMioska <quispechilojorge@gmail.com>
2024-01-13 09:34:29 -0500

    blog page content + paddings

--- src/helpers/data/posts.ts
+++ src/helpers/data/posts.ts
@@ -14,6 +14,8 @@ export const posts: PostInterface[] = [
     readingTime: 12,
     category: "Design",
     author: "Alice Johnson",
+    content:
+      "<h2>Maecenas fringilla imperdiet malesuada</h2> <p>Phasellus tristique dignissim sapien vel consectetur. Nam in lacus iaculis, euismod erat eget, posuere sem. Praesent ac sapien gravida, lobortis enim eget, blandit felis. Pellentesque hendrerit turpis quis libero aliquam pulvinar. Maecenas fringilla imperdiet malesuada.</p><p>Suspendisse in ullamcorper turpis, ut ultrices neque. Nulla eleifend magna a eros mattis volutpat. Nam a purus purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at efficitur odio, nec tempus nisl. Ut finibus venenatis nulla id maximus. Vestibulum pretium, risus vel fringilla laoreet.</p><h2>Ut finibus venenatis nulla</h2><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Morbi pretium consequat consectetur. Praesent blandit ornare felis a blandit. Sed lacinia bibendum mollis. Nunc vel tellus volutpat, faucibus.</p><p>Phasellus tristique dignissim sapien vel consectetur. Nam in lacus iaculis, euismod erat eget, posuere sem. Praesent ac sapien gravida, lobortis enim eget, blandit felis. Pellentesque hendrerit turpis quis libero aliquam pulvinar. Maecenas fringilla imperdiet malesuada. Vestibulum cursus malesuada aliquam. Aliquam lacinia quam condimentum placerat congue. Maecenas tempor tristique odio non auctor. Nunc hendrerit in eros.</p>",
   },
   {
     id: 2,
@@ -24,6 +26,8 @@ export const posts: PostInterface[] = [
     readingTime: 15,
     category: "Analytics",
     author: "Jane Smith",
+    content:
+      "<h2>Maecenas commodo nisl eu sapien.</h2> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Nam luctus id diam ut pharetra. Ut mi elit, ultrices accumsan pellentesque at, condimentum ac lectus. Praesent venenatis augue sed est fringilla, in condimentum sapien tincidunt. Nullam lacinia nisi sit amet felis suscipit bibendum. Etiam sed semper tellus.</p><p class='tracking-widest'>Suspendisse in ullamcorper turpis, ut ultrices neque. Nulla eleifend magna a eros mattis volutpat. Nam a purus purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at efficitur odio, nec tempus nisl. Ut finibus venenatis nulla id maximus. Vestibulum pretium, risus vel fringilla laoreet.</p><h2>Esse cillum dolore eu fugiat nulla pariatur</h2><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Morbi pretium consequat consectetur. Praesent blandit ornare felis a blandit. Sed lacinia bibendum mollis. Nunc vel tellus volutpat, faucibus.</p>",
   },
   {
     id: 3,
@@ -35,6 +39,6 @@ export const posts: PostInterface[] = [
     category: "Development",
     author: "John Doe",
     content:
-      "<h2>Lorem Ipsum Title</h2> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><h2>Another Lorem Ipsum Title</h2><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
+      "<h2>In pharetra, ligula nec pulvinar</h2> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Suspendisse in ullamcorper turpis, ut ultrices neque. Nulla eleifend magna a eros mattis volutpat. Nam a purus purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at efficitur odio, nec tempus nisl. Ut finibus venenatis nulla id maximus. Vestibulum pretium, risus vel fringilla laoreet.</p><h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Morbi pretium consequat consectetur. Phasellus tristique dignissim sapien vel consectetur. Nam in lacus iaculis, euismod erat eget, posuere sem. Praesent ac sapien gravida, lobortis enim eget, blandit felis. Pellentesque hendrerit turpis quis libero aliquam pulvinar. Maecenas fringilla imperdiet malesuada. Vestibulum cursus malesuada aliquam. Aliquam lacinia quam condimentum placerat congue. Maecenas tempor tristique odio non auctor. Nunc hendrerit in eros.</p>",
   },
 ];
--- src/routes/BlogPage.tsx
+++ src/routes/BlogPage.tsx
@@ -32,53 +32,14 @@ export const BlogPage = () => {
       </div>
       <span className="text-lg py-5">{post?.description}</span>
       <img
-        className="w-full md:w-[80%] h-[25rem] object-cover"
+        className="w-full md:w-[80%] h-[25rem] object-cover pb-5"
         src={post?.img}
         alt="Post Image"
       />
-
-      <h2 className="text-2xl">Title</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-        voluptate veritatis, architecto, molestias deleniti, quam sapiente dicta
-        veniam nemo laboriosam placeat porro necessitatibus corrupti itaque
-        optio tempora incidunt. Reprehenderit impedit dolorum error nemo aliquid
-        illum sit eligendi dolores beatae, quae corrupti fuga doloremque sunt,
-        rerum quo saepe commodi nobis repellat.
-      </p>
+      <div className="post-content-container">
+        {post?.content ? <>{parse(post?.content)}</> : "Loading..."}
+      </div>
       <Invitation />
-      <h2 className="text-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing.
-      </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem enim
-        quaerat minima ducimus praesentium ipsam reiciendis quibusdam velit
-        dignissimos. Sit quaerat distinctio quod expedita labore, eum fuga!
-        Blanditiis, expedita saepe. Corporis, et sit quam impedit aliquam
-        deserunt libero quia eligendi veritatis exercitationem laboriosam ipsum
-        doloribus. Nihil mollitia error vel libero deleniti rerum architecto
-        impedit repellendus, aut quidem? Mollitia, perferendis dolor! Dolor quos
-        exercitationem est soluta pariatur ducimus magni rem, nobis sit fugit ea
-        ut incidunt amet in labore nihil! Voluptatibus harum natus excepturi
-        reiciendis consequatur soluta eligendi adipisci velit laborum.
-        Asperiores dicta pariatur ex maxime nihil nobis qui vel? Mollitia, earum
-        eligendi. Unde laudantium nesciunt quas ipsam necessitatibus iure nihil
-        et. Voluptatibus deserunt maiores quae sed quam aliquid repellendus
-        provident? Debitis, ut vero! Molestias consectetur ipsa voluptates porro
-        suscipit obcaecati at. Earum tempora expedita quis blanditiis, quasi
-        saepe ipsam, nesciunt aut numquam tempore consequatur animi rerum
-        pariatur. A, possimus deserunt. Suscipit odio sed esse laudantium
-        inventore, eos minima rerum, non, nobis iste corrupti eum quisquam fugit
-        debitis similique incidunt aperiam pariatur magni reiciendis natus nam
-        porro! Illo, explicabo nobis. Placeat! Ex hic perspiciatis repellendus
-        reprehenderit deleniti vel. Iusto sed atque accusantium illo, nulla
-        esse, quam dolorum nisi quis ipsa ut quasi similique voluptatum ducimus,
-        enim error recusandae. Commodi, temporibus accusamus. Natus eaque ut
-        suscipit minus perferendis laborum, omnis recusandae modi reiciendis
-        dolorum molestiae explicabo debitis rem sit odit? Eum, nobis error quasi
-        numquam officia mollitia officiis doloribus facilis fuga vitae.
-      </p>
-      {post?.content ? <div>{parse(post?.content)}</div> : "No Content"}
     </article>
   );
 };
